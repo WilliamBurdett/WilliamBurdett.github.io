e361a39c743176e3e6aedeb258c146fd47933f36
William Burdett <william.burdett1@gmail.com>
2020-12-29 15:17:26 -0500

    Add extension

---
+++ firefox_extension/extract.js
@@ -0,0 +1,152 @@
+function upsert(array, name, value) {
+    if (value === "") {
+        return;
+    }
+    let found = false;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].name === name) {
+            if (array[i].value < value) {
+                array[i].value = value;
+            }
+            found = true;
+        }
+    }
+    if (!found) {
+        array.push({"name": name, "value": value});
+    }
+}
+
+function get(array, name){
+    console.log(name);
+    for(let i=0;i<array.length;i++) {
+        if (array[i].name === name){
+            return array[i].value;
+        }
+    }
+}
+
+function get_attributes() {
+    function parse_value(value){
+        let parsed_value;
+        parsed_value = value.replace("+", "");
+        parsed_value = parsed_value.replace("%", "");
+        parsed_value = parseFloat(parsed_value);
+        return parsed_value;
+    }
+
+    function get_stats(divs, stats) {
+        for (let i = 0; i < divs.length; i++) {
+            let div = divs[i];
+            let stat = div.getAttribute("stat");
+            if (stat !== null) {
+                let obj = {"name": stat};
+                if (div.classList.contains("text")) {
+                    // Resistance
+                    obj.value = div.innerText;
+                } else {
+                    // Default attribute
+                    let children = div.children;
+                    for (let j = 0; j < children.length; j++) {
+                        let child = children[j];
+                        if (child.classList.contains("value")) {
+                            obj.value = child.innerText;
+                        }
+                    }
+                }
+                if (obj.value) {
+                    upsert(stats, obj.name, parse_value(obj.value));
+                }
+            }
+        }
+    }
+
+    function to_string(array_of_obj) {
+        let output = "[\n"
+        array_of_obj.forEach(obj => {
+            output += "\t{\"name\": \"" + obj.name + "\", \"value\": \"" + obj.value + "\"},\n";
+        });
+        output += "]"
+        return output;
+    }
+
+    async function copy_to_clipboard(output) {
+        await navigator.clipboard.writeText(output)
+            .then(() => {
+                alert("Text copied to clipboard");
+            })
+            .catch(err => {
+                alert("Error in copying text: " + err);
+            });
+    }
+
+    let stats = [];
+    let divs = document.getElementsByTagName("div");
+    let first_div;
+    for (let i = 0; i < divs.length; i++) {
+        let div = divs[i];
+        let tab = div.getAttribute("tab");
+        if (tab !== null) {
+            if (!first_div){
+                first_div = div;
+            }
+            div.click();
+            get_stats(divs, stats);
+        }
+    }
+    first_div.click();
+    let output = []
+    let max_res = 0;
+    let min_res = 100;
+    let max_damage_modifier = 0;
+    let res_names = [
+        "resFire",
+        "resLightning",
+        "resCold",
+        "resPoison",
+        "resPierce",
+        "resBleeding",
+        "resLife",
+        "resAether",
+        "resStun",
+        "resChaos"
+    ];
+    let needed_stats = [
+        "health",
+        "oa",
+        "da",
+        "healthRegen",
+        "ar"
+    ];
+    stats.forEach(stat => {
+        if (res_names.includes(stat.name)) {
+            if (stat.value > max_res) {
+                max_res = stat.value;
+            }
+            if (stat.value < min_res) {
+                min_res = stat.value;
+            }
+        }
+        if (needed_stats.includes(stat.name)) {
+            upsert(output, stat.name, stat.value);
+        }
+        if (stat.name.startsWith("damage") && stat.name.endsWith("Modifier")) {
+            if (stat.value > max_damage_modifier) {
+                max_damage_modifier = stat.value;
+            }
+        }
+    })
+    upsert(output, "max_res", max_res);
+    upsert(output, "min_res", min_res);
+    upsert(output, "max_damage_modifier", max_damage_modifier);
+
+    let output_text = get(output, "max_damage_modifier") + "\t" +
+        get(output, "health") + "\t" +
+        get(output, "healthRegen") + "\t" +
+        get(output, "ar");
+
+    // console.log(to_string(stats));
+    // alert(to_string(output));
+    alert(output_text);
+    alert(get(output, "oa") + "\t" + get(output, "da"));
+}
+setTimeout(() => get_attributes(), 1000);
