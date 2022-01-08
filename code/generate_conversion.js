e5f2d3ccdbe848a3b0f0982e6604313fe6ee57a6
William Burdett <william.burdett1@gmail.com>
2022-01-08 11:26:07 -0500

    Adding conversion notes

---
+++ code/generate_conversion.js
@@ -0,0 +1,11 @@
+/*
+    https://www.youtube.com/watch?v=Nqf4PLpMNOU&list=PL-7HuL5PyGEKk8edrhehe9twkci1162Xp
+    Order:
+    1. Damage Skill Modifiers
+    2. Local Skill Conversion (Items + Skill Modifier)
+    3. Global Conversion (Auras + Items)
+        A. Elemental And Fire/Cold/Lightning to X multiply
+        A. Damage to Elemental And Fire/Cold/Lightning Additive
+    4. Armor Piercing (Only skills that have % weapon damage)
+
+ */
